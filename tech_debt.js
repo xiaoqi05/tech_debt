d8192794549eb603fba30ff0566aae7259ffb2c7
qxiao <qxiao@thoughtworks.com>
2018-12-21 10:01:13 +0800

    将数据抽取出来

--- tech_debt.js
+++ tech_debt.js
@@ -1,189 +1,173 @@
 var dom = document.getElementById("container");
 var myChart = echarts.init(dom);
-var app = {};
-option = null;
-var sourceData = [{
-    name: 'sonar',
-    difficulty: 14.56,
-    description: '描述信息',
-    importance: 64.5
-}, {
-    name: '王小明',
-    difficulty: 47.14,
-    description: '描述信息',
-    importance: 76.2
-}, {
-    name: '王小乐',
-    difficulty: 42.1,
-    description: '描述信息',
-    importance: 34.8
-}, {
-    name: '王小波',
-    difficulty: 63.2,
-    description: '描述信息',
-    importance: 97.4
-}, {
-    name: '王小龙',
-    difficulty: 97.3,
-    description: '描述信息',
-    importance: 67.5
-}, {
-    name: '王小宇',
-    difficulty: 78.1,
-    description: '描述信息',
-    importance: 37.7
-}, {
-    name: '王小禾',
-    difficulty: 41.1,
-    description: '描述信息',
-    importance: 12.8
-}];
+var option = null;
 
-var seriesData = sourceData.map(function (item, index, array) {
-    return {
-        name: item['name'],
-        value: [item['difficulty'], item['importance'], item['description']]
-    }
-});
+init();
 
-option = {
-    title: {
-        text: '技术债',
-        left: 'center'
-    },
-    tooltip: {
-        trigger: 'item',
-        axisPointer: {
-            show: true,
-            type: 'cross',
-            lineStyle: {
-                type: 'dashed',
-                width: 1
+function init() {
+    loadJSON(function (response) {
+        var sourceData = JSON.parse(response);
+        var seriesData = sourceData.map(function (item) {
+            return {
+                name: item['name'],
+                value: [item['difficulty'], item['importance'], item['description']]
+            }
+        })
+        option = {
+            title: {
+                text: '技术债',
+                left: 'center'
             },
-        },
-        formatter: function (obj) {
-            if (obj.componentType === "series") {
-                return '<div style="border-bottom: 1px solid rgba(255,255,255,.3); font-size: 18px;padding-bottom: 7px;margin-bottom: 7px">' +
-                    obj.name +
-                    '</div>' +
-                    '</span>' + obj.data.value[2]
-            }
-        }
-    },
-    label: {
-        normal: {
-            show: true,
-            fontSize: 10,
-            color: '#111111',
-            symbolSize: [60, 22],
-            position: 'inside',
-            formatter: function (params) {
-                return params.name
-            }
-        }
-    },
-    itemStyle: {
-        normal: {
-            color: '#fff',
-            borderColor: '#333333'
-
-        }
-    },
-
-    xAxis: {
-        name: 'DIFFICULTY',
-        type: 'value',
-        nameLocation: 'middle',
-        scale: true,
-        nameTextStyle: {
-            fontWeight: 'bold',
-            fontSize: 15,
-            fontStyle: 'italic'
-        },
-        axisLabel: {
-            formatter: '{value}'
-        },
-        splitLine: {
-            show: false
-        },
-        interval: 100,
-        boundaryGap: false,
-        axisLine: {
-            lineStyle: {
-                color: '#3259B8'
-            }
-        },
-        axisTick: {
-            show: false,
-            interval: 100
-        },
-
-    },
-    yAxis: {
-        name: 'IMPORTANCE',
-        type: 'value',
-        scale: true,
-        nameTextStyle: {
-            fontWeight: 'bold',
-            fontSize: 15,
-            fontStyle: 'italic'
-        },
-        nameLocation: 'middle',
-        axisLabel: {
-            formatter: '{value}'
-        },
-        splitLine: {
-            show: false
-        },
-        interval: 100,
-        axisLine: {
-            lineStyle: {
-                color: '#3259B8'
-            }
-        },
-        axisTick: {
-            show: false,
-            interval: 100
-        }
-    },
-    toolbox: {
-        feature: {
-            dataZoom: {}
-        }
-    },
-
-    series: [{
-        type: 'scatter',
-        data: seriesData,
-        symbol: 'rect',
-        symbolSize: [60, 25],
-        markLine: {
-            lineStyle: {
+            tooltip: {
+                trigger: 'item',
+                axisPointer: {
+                    show: true,
+                    type: 'cross',
+                    lineStyle: {
+                        type: 'dashed',
+                        width: 1
+                    },
+                },
+                formatter: function (obj) {
+                    if (obj.componentType === "series") {
+                        return '<div style="border-bottom: 1px solid rgba(255,255,255,.3); ' +
+                            'font-size: 18px;padding-bottom: 7px;margin-bottom: 7px">' +
+                            obj.name +
+                            '</div>' +
+                            '</span>' + obj.data.value[2]
+                    }
+                }
+            },
+            label: {
                 normal: {
-                    color: "#626c91",
-                    type: 'solid',
-                    width: 1,
+                    show: true,
+                    fontSize: 10,
+                    color: '#111111',
+                    symbolSize: [60, 22],
+                    position: 'inside',
+                    formatter: function (params) {
+                        return params.name
+                    }
                 }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#fff',
+                    borderColor: '#333333'
+
+                }
+            },
+
+            xAxis: {
+                name: 'DIFFICULTY',
+                type: 'value',
+                nameLocation: 'middle',
+                scale: true,
+                nameTextStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                    fontStyle: 'italic'
+                },
+                axisLabel: {
+                    formatter: '{value}'
+                },
+                splitLine: {
+                    show: false
+                },
+                interval: 100,
+                boundaryGap: false,
+                axisLine: {
+                    lineStyle: {
+                        color: '#3259B8'
+                    }
+                },
+                axisTick: {
+                    show: false,
+                    interval: 100
+                },
 
             },
-            data: [{
-                xAxis: 50,
-                itemStyle: {
-                    normal: {
-                        color: "transparent",
+            yAxis: {
+                name: 'IMPORTANCE',
+                type: 'value',
+                scale: true,
+                nameTextStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                    fontStyle: 'italic'
+                },
+                nameLocation: 'middle',
+                axisLabel: {
+                    formatter: '{value}'
+                },
+                splitLine: {
+                    show: false
+                },
+                interval: 100,
+                axisLine: {
+                    lineStyle: {
+                        color: '#3259B8'
                     }
+                },
+                axisTick: {
+                    show: false,
+                    interval: 100
                 }
-            }, {
-                yAxis: 50,
-                itemStyle: {
-                    normal: {
-                        color: "transparent",
-                    }
+            },
+            toolbox: {
+                feature: {
+                    dataZoom: {}
+                }
+            },
+
+            series: [{
+                type: 'scatter',
+                data: seriesData,
+                symbol: 'rect',
+                symbolSize: [60, 25],
+                markLine: {
+                    lineStyle: {
+                        normal: {
+                            color: "#626c91",
+                            type: 'solid',
+                            width: 1,
+                        }
+
+                    },
+                    data: [{
+                        xAxis: 50,
+                        itemStyle: {
+                            normal: {
+                                color: "transparent",
+                            }
+                        }
+                    }, {
+                        yAxis: 50,
+                        itemStyle: {
+                            normal: {
+                                color: "transparent",
+                            }
+                        }
+                    }]
                 }
             }]
+        };
+        if (option && typeof option === "object") {
+            myChart.setOption(option, true);
         }
-    }]
-};
-if (option && typeof option === "object") {
-    myChart.setOption(option, true);
+
+    });
+}
+
+function loadJSON(callback) {
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.overrideMimeType("application/json");
+    xmlHttp.open('GET', 'data.json', true);
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            callback(xmlHttp.responseText);
+        }
+    };
+    xmlHttp.send(null);
 }
